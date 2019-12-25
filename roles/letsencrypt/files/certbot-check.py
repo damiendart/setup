5487e3fbcc8078c5254a68e6e77b1dcd6d382fe2
Damien Dart <damiendart@pobox.com>
2019-12-25 09:45:58 +0000

    Add and update file comments.

--- roles/letsencrypt/files/certbot-check.py
+++ roles/letsencrypt/files/certbot-check.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# This file was written by Damien Dart, <damiendart@pobox.com>. This is
+# free and unencumbered software released into the public domain. For
+# more information, please refer to the accompanying "UNLICENCE" file.
+
 
 from collections import Counter
 import re
