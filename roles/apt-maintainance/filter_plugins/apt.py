71ddaaff37527eeb9fef1c5fb5fe017a0b4ccba7
Damien Dart <damiendart@pobox.com>
2020-03-29 17:53:14 +0100

    Add Jinja filter to clean up APT dry-run output.

---
+++ roles/apt-maintainance/filter_plugins/apt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# This file was written by Damien Dart, <damiendart@pobox.com>. This is
+# free and unencumbered software released into the public domain. For
+# more information, please refer to the accompanying "UNLICENCE" file.
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'apt_simulate_output_cleanup': self.apt_simulate_output_cleanup
+        }
+
+    def apt_simulate_output_cleanup(self, lines):
+        output = []
+
+        for line in lines:
+            if line.startswith('Inst'):
+                words = line.split(' ')
+
+                new_line = '{0}: {1} => {2}'.format(
+                    words[1],
+                    words[2][1:-1],
+                    words[3][1:]
+                )
+                output.append(new_line)
+
+            elif line.startswith('Remv'):
+                words = line.split(' ')
+                output.append(words[0])
+
+        return sorted(output)
